964b96e4741cf317af0364bcca7d7632f519b04c
HyeonSeop <94344796+leehyeonseop@users.noreply.github.com>
2022-04-28 22:49:12 +0900

    Add files via upload

---
+++ Javascript/002.js
@@ -0,0 +1,11 @@
+document.getElementById("one").innerHTML =
+"hello <strong>world document</strong>";
+console.log("hello world console");
+console.error("error~");
+console.info("hello info");
+console.table({
+one: "two",
+three: "four",
+});
+window.alert("hello world alert");
+document.write("hello!!");
